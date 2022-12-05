ea77431df96e245e7c6b66ccca29f999a1420134
vikash05/07/1996 <vikashrajpoot2995@gmail.com>
2022-12-05 18:33:09 +0530

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const route = require("./routes/route.js")
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+mongoose.connect("mongodb+srv://ManojKoli:ManojKoli@cluster0.kwqvp.mongodb.net/group30Database?authSource=admin&replicaSet=atlas-sncxo8-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true",
+{useNewUrlparser:true})
+.then(()=>console.log("MongoDb is connected"))
+.catch(err =>console.log(err))
+
+app.use('/',route);
+
+app.listen(process.env.PORT ||3000,function(){
+    console.log("Express is running on port ",+(process.env.Port || 3000))
+
+});
---
+++ models/bookModel.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+
+const bookModel = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        trim:true
+        
+    },
+    excerpt: {
+        type :String,
+        required: true,
+        trim:true
+    },
+    userId: {
+        type: ObjectId,
+        required: true,
+        ref: "usermbook",
+        trim:true
+    }, 
+    ISBN: {
+        type: String,
+        required: true,
+        unique: true,
+        trim:true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim:true,
+        lowercase:true
+    },
+    subcategory: {
+        type: String,
+        required: true,
+        trim:true,
+        lowercase:true
+    },
+    reviews: {
+        type: Number,
+        default:0,
+        required:true
+        
+       
+    }, 
+    deletedAt: {
+        type: Date,
+        default: ""
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    releasedAt: {
+        type: Date,
+      required: true,
+
+    }, 
+},{timestamps:true});
+
+module.exports = mongoose.model("Book", bookModel);
